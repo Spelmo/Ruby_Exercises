916375fb6c3ffdf2b386f1dd20f13e8f27373bb0
Spelmo <42944289+Spelmo@users.noreply.github.com>
2018-09-28 13:55:17 +0200

    Update exo_16.rb

--- Vendredi_Ruby/exo_16.rb
+++ Vendredi_Ruby/exo_16.rb
@@ -6,9 +6,12 @@ user_age = 0
 howlong = 2018-user_year
 
 while user_year != 2018
- puts "Il y a  #{howlong} ans, vous aviez #{user_age} ans"
- user_year = user_year+1
- user_age = user_age +1
- howlong = 2018-user_year
+  if user_age == howlong
+    then puts "Il y a #{howlong} ans, vous aviez la moitié de l'âge que vous avez aujourd'hui."
+    else puts "Il y a  #{howlong} ans, vous aviez #{user_age} ans"
+    end
+   user_year = user_year+1
+   user_age = user_age +1
+   howlong = 2018-user_year
 end
  puts "Cette année vous avez #{user_age} ans"
